a2cf536e945cab91db9976d568b64884f1cc2d92
Kevin Holt <kevin2holt@gmail.com>
2022-09-30 15:33:11 -0400

    Added interaction with mongodb

--- index.js
+++ index.js
@@ -1,324 +1,62 @@
 const express = require("express");
+const mongoose = require("mongoose");
+// const morgan = require("morgan");
+const Course = require("./models/course");
 
 // express app
 const app = express();
 
+// Connect to MongoDB
+const dbURI = "mongodb+srv://team4:OU5Acjz3VOzsbeHw@sdev255-team4-groupproj.tany3x9.mongodb.net/SDEV255-Team4?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+	.then(() => app.listen(3000))
+	.catch((err) => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests on port 3000
-app.listen(3000);
+// Middleware & Static Files
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+// app.use(morgan("dev"));
 
 //Home
 app.get("/", (req, res) => {
-  res.render("index", { title: "Code University" });
+	res.render("index", { title: "Code University" });
 });
 
 //Courses
 app.get("/courses", (req, res) => {
-  const courses = [
-    // This is here simply to act as a placeholder for the actual data
-    // It is copy/paste repeated to illustrate the scroll on the table
-    // If possible, I would recommend folding this array
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV240",
-      dept_id: "SDEV",
-      course_id: "240",
-      credit_hours: 3,
-      name: "Software Development in C#",
-      desc: "Learn the C# language",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-    {
-      course_code: "SDEV255",
-      dept_id: "SDEV",
-      course_id: "255",
-      credit_hours: 3,
-      name: "Web Application Development",
-      desc: "Create applications on the web!",
-    },
-  ];
-  res.render("courses", { title: "courses", courses });
+	Course.find()
+		.then((result) => res.render("courses", {title: "Courses", courses: result}))
+		.catch((err) => console.log(err));
+});
+
+app.get("/courses/:id", (req, res) => {
+	const id = req.params.id;
+	Course.findById(id)
+		.then((result) => {
+			console.log(result);
+			res.render("courseDetails", {title: "Course Details", course: result});
+		})
+		.catch((err) => console.log(err));
+});
+
+app.post("/courseCreate", (req, res) => {
+	const course = new Course(req.body);
+
+	course.save()
+		.then((result) => res.redirect("/courses"))
+		.catch((err) => console.log(err));
 });
 
 //Extra I haven't gotten rid of
 app.get("/link", (req, res) => {
-  res.render("link", { title: "Link" });
+	res.render("link", { title: "Link" });
 });
 
 // 404 page
 // app.use() needs to go at the end, after all app.get()
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+	res.status(404).render("404", { title: "404" });
 });
